030a929d72383f06cf820f3766b523e045516cec
souyana <colorfulparka@gmail.com>
2025-12-02 17:49:17 -0800

    Add custom color theme in kanagawa colorscheme plugin

--- init.lua
+++ init.lua
@@ -912,6 +912,7 @@ require('lazy').setup({
     },
   },
 
+  -- Color scheme plugins
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   {
@@ -923,11 +924,32 @@ require('lazy').setup({
         dark = 'dragon',
         light = 'lotus',
       },
-      transparent = false,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Borderless Telescope
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+          -- Transparent Floating Windows
+          NormalFloat = { bg = 'none' },
+          FloatBorder = { bg = 'none' },
+          FloatTitle = { bg = 'none' },
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        }
+      end,
     },
   },
 
